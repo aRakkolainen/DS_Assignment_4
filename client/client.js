7ca3d41e639a6ba94c032d449873e5708a2e8856
aRakkolainen <aino.rakkolainen@gmail.com>
2024-04-04 20:19:09 +0300

    global messages working

--- client/client.js
+++ client/client.js
@@ -6,89 +6,154 @@ const readLine = require('node:readline').createInterface({
     input: process.stdin, 
     output: process.stdout
 })
-
-const waitForUsername = new Promise(resolve => {
-    readLine.question("Give your username to enter chat: ", username => {
+let waitForUsername;
+let usernameAccepted = false; 
+waitForUsername = new Promise((resolve) => {
+    readLine.question("Give username to enter chat (if name is already taken, connection is closed): ", username => {
         resolve(username)
+    });  
+})
+
+waitForUsername.then((username) => {
+    const socket = net.createConnection({
+        port: 8000,
+        host: process.argv[2] ?? "localhost"
     });
+
+    socket.on('connect', () => {
+        socket.write(`${username} has joined the chat.`);
+        console.log("Your options:")
+        console.log("1) Join global chat to send messages to everyone")
+        console.log("2) Send private messages to someone")
+        console.log("0) Close the chat")
+        console.log("Write number of your choice anytime in the chat")
+    });
+    let option = null; 
+    readLine.on('line', data => {
+        if (data === "1") {
+            option = 1;
+            console.log("Joining global chat")
+        } else if(data === "2") {
+            option = 2;
+            console.log("Starting private chat");
+            //socket.write("2");
+        } else if(data === "0") {
+            option = 0; 
+        }
+        if (option == 1) {
+            socket.write(`1-${username}: ${data}`)
+        } else if(option == 2) {
+            socket.write(`2-${username}: ${data}`)
+            const waitForRecipientName = new Promise((resolve) => {
+                readLine.question("Send your message in form: recipient: your message", recipient => {
+                    resolve(recipient)
+                });
+            })
+            waitForRecipientName.then((recipient) =>
+                socket.write(`2-${username}:${recipient}`)
+            )
+        } else if (option == 0) {
+            socket.write(`${username} has left the chat.`)
+            socket.setTimeout(1000);
+        }
+        //socket.write(`${username}: ${data}`)
+    })
+
+    socket.on('data', data => {
+        //Showing messages only if the user has joined some chat.. (HEH EI TOIMI :D)
+        if (data === "Username accepted.") {
+            usernameAccepted = true;
+        }
+        console.log("\n%s", data) 
+        
+        
+        //console.log("\n%s", data)
+    })
+
+    socket.on('end', () => {
+        process.exit();
+    })
+
+    socket.on('timeout', () => {
+        socket.write("0");
+        socket.end();
+    })
+
+
+    socket.on('error', () => {
+        console.log("Shutting down due error");
+    })
+
+
+    
 })
 
 
 
+
+
+
+
+/*
 waitForUsername.then(username => {
     const socket = net.connect({
-        port: 8000
+        port: 8000,
+        host: process.argv[2] ?? "localhost"
+    })
+    socket.write(username);
+    socket.on("data", (data) => {
+        console.log("\n%s", data);
+        if (data.toString() === "Name is taken, try again") {
+            new Promise(resolve => {
+                readLine.question("Give username to enter chat: ");
+            })
+        }
+    })
+})
+*/
+//Connecting to ip address given by client: https://cs.lmu.edu/~ray/notes/jsnetexamples/
+/*waitForUsername.then(username => {
+    const socket = net.connect({
+        port: 8000,
+        host: process.argv[2] ?? "localhost"
     });
     socket.on("connect", () => {
         let text = username + " has joined the chat.";
         socket.write(text);
-        console.log("What do you want to do?")
+        /*console.log("What do you want to do?")
         console.log("1) Send message to everyone")
         console.log("2) Send private message to someone")
-        console.log("0) Quit")
-    })
-
-    /*const waitForDecision = new Promise(resolve => {
-        console.log("What do you want to do?")
-        console.log("1) Send message to everyone")
-        console.log("2) Send private message to someone")
-        console.log("0) Quit")
-        readLine.question("Give your choice: ", choice => {
-            resolve(choice)
-        })
-    })*/
-
-   /* waitForDecision.then(choice => {
-        if (choice === "1") {
-            console.log("Sending message to everyone");
-            readLine.on('line', data => {
-                let text = username + ": " + data; 
-                socket.write(text);
-            })
-        } else if (choice === "2") {
-            const waitForRecipientName = new Promise(resolve => {
-                readLine.question("Give name who you want to sent message to: ", name => {
-                    resolve(name)
-                })
-            })
-
-            waitForRecipientName.then(name => {
-                socket.write(name)
-            })
-        } else if(choice === "0") {
-            let text = username +  " has left the chat.";
-            readLine.on('line', data => {
-                socket.write(text);
-                socket.setTimeout(1000);
-            })
-        }
-    })*/
+        console.log("0) Quit")*/
+    /*})
     readLine.on('line', data => {
-        if (data === "1") {
+        let choice = null;
+        let text; 
+        /*if (data === "1") {
             console.log("Opening global chat..")
-            let text = "1" + username + ": " + data; 
-            socket.write(text);
-        } else if(data === 0) {
-            let text = username +  " has left the chat.";
-            socket.write(text);
-            socket.setTimeout(1000);
-        } else if(data === 2) {
+            choice = 1;
+            text = choice.toString() + ":" + username + ": " + data
+        } else if(data === "2") {
             console.log("Who you want to send message to?")
             const waitForRecipientName = new Promise(resolve => {
                 readLine.question("Give name who you want to sent message to: ", name => {
                     resolve(name)
                 })
             })
-
             waitForRecipientName.then(name => {
                 socket.write(name)
             })
+        } else if(data === 0) {
+            let text = username +  " has left the chat.";
+            socket.write(text);
+            socket.setTimeout(1000);
         }
-        })
-
-
+        //socket.write(text);
+    })
     socket.on('data', data => {
         console.log("\n%s", data);
+        if (data.toString() === "Username is taken. Try again") {
+            console.log("Ask for username again..");
+        }
     })
 
     socket.on('timeout', () => {
@@ -105,13 +170,4 @@ waitForUsername.then(username => {
         console.log("Shutting down due error");
     })
 })
-
-/*const client = net.createConnection(options, () => {
-    console.log('connected to server!');
-    client.write('hello\r\n');
-})
-
-client.on('data', (data) => {
-    //onsole.log(data.toString());
-    client.end();
-})*/
+*/
--- server/server.js
+++ server/server.js
@@ -1,21 +1,73 @@
 //Based on this tutorial: https://tnickel.de/2020/03/29/2020-03-nodejs-http-server-using-the-net-module/
 // Hint: https://www.youtube.com/watch?v=4yPnp4k8VMA
 //This program is based on this tutorial by WittCode: https://www.youtube.com/watch?v=-rVxORKWzv0
+//Server is based on this ChatServer tutorial: https://cs.lmu.edu/~ray/notes/jsnetexamples/
 // and the documentation of Node.js
 
 const net = require('node:net');
-let sockets = [];
+let clientSockets = [];
+let clientNames = [];
+let globalChats = [];
+let privateChats = []; 
+
 
 const server = net.createServer((socket) => {
-    sockets.push(socket);
+    //socket.write("Give your username to enter the chat: ");
     console.log("New client connected");
-    socket.on('data', data =>{
-        if (data.includes("1")) {
-            broadcast(data, socket);
+    let client = null;
+    let recipient = null;
+    let option = null; 
+    let firstTime = false; 
+    socket.on('data', (data) =>{
+        const message = data.toString("utf-8").trim();
+        //splitting the username part
+        //Checking if name is already defined and is it already reserved to be this specific one
+        if (message.includes("has joined the chat.")) {
+            let user = message.split(" ");
+            let username = user[0];
+            if (clientNames.includes(username)) {
+                socket.write("Username is taken.")
+            } else {
+                clientNames.push(username);
+                clientSockets.push(socket);
+                socket.write("Username accepted.");
+                //broadcast(message, socket);
+            }
+        } else {
+            let content = message.split("-");
+            let user = content[1].split(":");
+            let username = user[0];
+            let messageText = content[1];
+            option = content[0];
+            //let sender = content[1];
+            //console.log(content[0]);
+            //broadcast(`${username} has joined the chat.`, socket);
+            
+            switch(option) {
+                case "1":
+                    if (!content[1].includes(": 1") && firstTime == false){
+                        broadcast(`${username} has joined the chat.`, socket);
+                        broadcast(messageText, socket);
+                        firstTime = true;
+                    } else if(firstTime == true) {
+                        broadcast(messageText, socket);
+                    }
+                    break; 
+                case "2":
+                    let contentInfo = message.split(":");
+                    let sender = contentInfo[0].split("-")
+                    let senderName = sender[1]; 
+                    let recipient = contentInfo[1]; 
+                    let text = senderName + ": " + contentInfo[2];
+                    sendPrivateChat(text, socket, recipient);
+                    break;
+                case "0":
+
+                }
+            
+            
+
         }
-        let text = data.toString(); 
-        let user = text.split(" ");
-        sockets[sockets.indexOf(socket)].name = user[0];
     })
     socket.on('error', err =>  {
         console.log("A client has disconnected");
@@ -23,27 +75,57 @@ const server = net.createServer((socket) => {
 
     socket.on('end', () => {
         console.log('A client left.');
+        clientNames.delete(username);
+        clientSockets.delete(socket);
     })
 })
 server.listen(8000, () => {
     console.log("Listening to port 8000");
 });
 
-function broadcast(message, socketSent) {
-    if (message.toString() === "quit") {
-        let index = sockets.indexOf(socketSent);
-        sockets.splice(index, 1);
+/*function giveOptions() {
+    
+}*/
+
+
+function findRecipient(recipientName) {
+    console.log(recipientName);
+    if (recipientName && recipientName !== "2") {
+        let recipientIndex = clientNames.indexOf(recipientName); 
+        if (recipientIndex == -1) {
+        return null;
     } else {
-        sockets.forEach(socket => {
+        return clientSockets[recipientIndex]
+    }
+    }
+
+}
+
+function broadcast(message, socketSent) {
+    //What if here is checked whether the message is sent to everyone or not..
+    if (message.toString() === "quit") {
+        let index = clientSockets.indexOf(socketSent);
+        clientSockets.splice(index, 1);
+    } else {
+        console.log("sending message..")
+        clientSockets.forEach(socket => {
             //if(socket !== socketSent) {
-            if (socket != socketSent) socket.write(message);
+            if (socket !== socketSent) {
+                socket.write(message);
+                //globalChats.push({sender: sender, message: message})
+            }
         })
     }
 }
 
 
-function sendPrivateChat(message, socketSent, socketReceiver){
-    sockets.forEach(socket => {
-        console.log(socket.name)
-    })
+function sendPrivateChat(message, senderSocket, recipientName){
+    let recipientSocket = null;
+    recipientSocket= findRecipient(recipientName);
+    console.log(recipientSocket);
+    if (recipientSocket) {
+        recipientSocket.write(message);
+    } else {
+        senderSocket.write("Recipient not found!");
+    }
 }
